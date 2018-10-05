7b3acce602f8a61d4091f9ba1eef7a66a3dd2a8b
Malik Obeidin <mobeidin@illinois.edu>
2018-10-05 09:03:00 -0500

    Hand coded in the Pseudogroup corresponding to a genus 2 interval exchange with given Dehn-Thurston coordinates

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 from orbits import *
-G = Pseudogroup([ Flip([11,16],[13,18]), Shift([1,4],[3,6]), Shift([10,14],[14,18]), Shift([8,10],[14,16]) ], Interval(1,18))
-print 'G=', G
-print 'Reducing G:', G.reduce(), 'orbits.'
-print
+#G = Pseudogroup([ Flip([11,16],[13,18]), Shift([1,4],[3,6]), Shift([10,14],[14,18]), Shift([8,10],[14,16]) ], Interval(1,18))
+#print 'G=', G
+#print 'Reducing G:', G.reduce(), 'orbits.'
+#print
 
-H = Pseudogroup([ Flip([11,16],[13,18]), Shift([1,4],[3,6]), Shift([10,14],[14,18]), Shift([8,10],[14,16]), Flip([6,7],[8,9]) ], Interval(1,18))
+#H = Pseudogroup([ Flip([11,16],[13,18]), Shift([1,4],[3,6]), Shift([10,14],[14,18]), Shift([8,10],[14,16]), Flip([6,7],[8,9]) ], Interval(1,18))
+#print 'H=', H
+#print 'Reducing H:', H.reduce(), 'orbits.'
+
+
+
+a = MonoidElement('a', StringMonoid)
+b = MonoidElement('b', StringMonoid)
+c = MonoidElement('c', StringMonoid)
+d = MonoidElement('d', StringMonoid)
+e = MonoidElement('e', StringMonoid)
+
+H = Pseudogroup([ Flip([11,16],[13,18], a), Shift([1,4],[3,6], b), Shift([10,14],[14,18], c), Shift([8,10],[14,16], d), Flip([6,7],[8,9], e) ], StringMonoid, Interval(1,18))
 print 'H=', H
 print 'Reducing H:', H.reduce(), 'orbits.'
+
 
